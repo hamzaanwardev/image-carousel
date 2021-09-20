a3ab8e225de3ee5cf94b63569721f830aaa7d4db
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2021-09-20 23:58:36 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 const imgs = document.getElementById('imgs');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+
+
+const img = document.querySelectorAll('#imgs img');
